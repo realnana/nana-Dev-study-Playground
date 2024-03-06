ca74d78263422dcef8526b482f68d051f2eb0b00
Nyu0Kim <nyu0kim@gmail.com>
2024-03-06 20:47:58 +0900

    게시물 생성하기(Controller)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -11,5 +11,11 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
+    @Post()
+    createBoard(@Body('title') title: string, 
+                @Body('description') description: string
+    ): Board {
+        return this.boardsService.createBoard(title, description)
+    }
 
 }
