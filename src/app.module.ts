161b3679fe87b5a6e1cf068a5ed55ad55edfeea7
Nyu0Kim <nyu0kim@gmail.com>
2024-03-08 08:01:24 +0900

    Fix: class 'AppModule' implements keyword error 인터페이스 구현을 시도했으나 NestModule 구현에서 전체적인 코드와 맞지 않는 오류 발생 NestModule을 import 한 뒤 AppModule에서 클래스 구현, configure 메서드 내에서 Mongoose 디버그 모드를 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import * as mongoose from 'mongoose';
-import { LeaderboardModule } from './leaderboard/leaderboard.module';
-
-
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true,}), MongooseModule.forRoot(process.env.MONGODB_URI),
-    LeaderboardModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/leaderboard'),
   ],
 })
 export class AppModule implements NestModule {
-  private readonly isDev: boolean = process.env.NODE_ENV == 'dev' ? true : false;
+  private readonly isDev: boolean = process.env.NODE_ENV === 'dev';
+
   configure() {
     mongoose.set('debug', this.isDev);
   }
 }
+
