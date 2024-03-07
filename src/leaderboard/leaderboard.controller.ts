56f4a5a9509fc7dc309fae6d2e97cbacafa9af81
Nyu0Kim <nyu0kim@gmail.com>
2024-03-08 08:07:46 +0900

    Feat: add leaderboard controller logic 리더보드 컨트롤러를 구현하였음 @Param() 데코레이터를 사용하여 엑스트라 데이터를 객체로 받아왔음

--- src/leaderboard/leaderboard.controller.ts
+++ src/leaderboard/leaderboard.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+
+import { Controller, Post, Get, Param } from '@nestjs/common';
+import { LeaderboardService } from './leaderboard.service';
+import { LeaderboardUserList } from 'src/leaderboard-user-list/leaderboard-user-list';
 
 @Controller('leaderboard')
-export class LeaderboardController {}
+export class LeaderboardController {
+  constructor(private readonly leaderboardService: LeaderboardService) {}
+
+  @Post(':uid/:score/:uploadTime/:country/:name/:guildName')
+  async uploadScore(
+    @Param('uid') uid: string,
+    @Param('score') score: number,
+    @Param('uploadTime') uploadTime: string,
+    @Param('country') country: string,
+    @Param('name') name: string,
+    @Param('guildName') guildName: string,
+  ): Promise<LeaderboardUserList> {
+    const extraData = { country, name, guildName };
+    return await this.leaderboardService.uploadScore(uid, score, uploadTime, extraData);
+  }
+
+  @Get(':id')
+  async getLeaderboard(@Param('id') id: number): Promise<LeaderboardUserList[]> {
+    return await this.leaderboardService.getLeaderboard(id);
+  }
+}
