37ef0bbeb8ccf36b3d3477ff35fc1db888bdc91d
Nyu0Kim <nyu0kim@gmail.com>
2024-03-08 07:45:24 +0900

    Feat: leaderboard-user-list schema 리더보드 아이템 스키마를 'leaderboard-user-list.ts' 파일에 정의하였음 유저아이디, 점수, 점수업로드 시간, 엑스트라데이터인 나라코드, 이름, 길드이름만 우선 저장하였음

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 
+
 @Module({
-  imports: [LeaderboardModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true,}), MongooseModule.forRoot(process.env.MONGODB_URI),
+    LeaderboardModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.NODE_ENV == 'dev' ? true : false;
+  configure() {
+    mongoose.set('debug', this.isDev);
+  }
+}
--- src/leaderboard-user-list/leaderboard-user-list.ts
+++ src/leaderboard-user-list/leaderboard-user-list.ts
@@ -1,1 +1,25 @@
-export class LeaderboardUserList {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+export class LeaderboardUserList extends Document {
+    @Prop()
+    uid: string;
+
+    @Prop()
+    score: number;
+
+    @Prop()
+    uploadTime: string;
+
+    @Prop()
+    country: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    guildName: string;
+}
+
+export const LeaderBoardUserListSchema = SchemaFactory.createForClass(LeaderboardUserList);
