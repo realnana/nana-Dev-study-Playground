e2ed7843445d318faae183739211549537b847e0
Nyu0Kim <nyu0kim@gmail.com>
2024-03-08 22:52:44 +0900

    Feat: NestJS 프로젝트 초기 세팅 중 미리 해두면 좋은 설정 2 : ValidationPipe transformation - 입력된 데이터를 원하는 형식(타입)으로 변환 Validation - 입력된 데이터의 타입이나 형태가 기준에 유효하지 않을 때 예외처리 먼저 종속성 설치(자세한 내용 노션참조) 후 코드 작성하기 모든 endpoint에서 잘못된 데이터를 받지 않도록 보호 -> main.ts에 global로 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS 활성화
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
