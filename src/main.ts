c06711629a36f5fb69f2182134b46b2164bdb4f6
Nyu0Kim <nyu0kim@gmail.com>
2024-03-08 22:41:54 +0900

    Feat: NestJS 프로젝트 초기 세팅 중 미리 해두면 좋은 설정 1 : CORS 활성화 main.ts에 설정해주기 나중에 cookie 쓸 때 옵션을 더 줘야하지만 그 전까지 이정도로 충분히 커버 가능함

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS 활성화
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
