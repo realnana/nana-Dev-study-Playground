fab9bb6ece4613c5336857690feebe47799e40ec
Nyu0Kim <nyu0kim@gmail.com>
2024-03-09 00:08:59 +0900

    Feat: NestJS 프로젝트 초기 세팅 중 미리 해두면 좋은 설정 4 : Logger Nest에는 내부 logger가 제공되어 이 logger를 활용 필요시 error, warn, debug, verbose 등 추가 가능

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const PORT = configService.get('SERVER_PORT');
+
+  if (configService.get('NODE_ENV') === 'development') {
+    Logger.log(`Application running on port ${PORT}, http://localhost:${PORT}`);
+  }
 
   // CORS 활성화
   app.enableCors();
