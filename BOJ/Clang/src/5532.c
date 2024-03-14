6a0c2d4783796adce8e39ad9f5024d13b06eb3dd
Nyung <nyu0kim@gmail.com>
2024-03-14 11:17:20 +0900

    5532 C 함수분리

--- BOJ/Clang/src/5532.c
+++ BOJ/Clang/src/5532.c
@@ -1,10 +1,32 @@
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int L, A, B, C, D, num1 = 0, num2 = 0;
+//    scanf("%d %d %d %d %d", &L, &A, &B, &C, &D);
+//
+//    num1 = A / C;
+//    if (A % C != 0)
+//    {
+//        num1++;
+//    }
+//    num2 = B / D;
+//    if (B % D != 0)
+//    {
+//        num2++;
+//    }
+//    if (num1 >= num2)
+//    {
+//        printf("%d", L - num1);
+//    }
+//    else
+//    {
+//        printf("%d", L - num2);
+//    }
+//    return 0;
+//}
 
-int main()
-{
-    int L, A, B, C, D, num1 = 0, num2 = 0;
-    scanf("%d %d %d %d %d", &L, &A, &B, &C, &D);
-
+void numCheck() {
     num1 = A / C;
     if (A % C != 0)
     {
@@ -23,5 +45,10 @@ int main()
     {
         printf("%d", L - num2);
     }
+}
+
+int main() {
+    int L, A, B, C, D, num1 = 0, num2 = 0;
+    scanf("%d %d %d %d %d", &L, &A, &B, &C, &D);
     return 0;
 }
