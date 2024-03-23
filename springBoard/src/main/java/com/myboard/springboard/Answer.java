2879eea6cb8274b1d2ea0651cb23c71a0f9d377e
Nyung <nyu0kim@gmail.com>
2024-03-24 02:21:33 +0900

    Feat: 답변 엔티티 작성 질문 엔티티를 참조
    적용된 어노테이션
    @OneToMany - mappedBy
    Answer 속성 List 형태로 구현

---
+++ springBoard/src/main/java/com/myboard/springboard/Answer.java
@@ -0,0 +1,28 @@
+package com.myboard.springboard;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
