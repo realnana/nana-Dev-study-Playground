ca41519b588445c7009858e57df54efe4348ce7e
Nyung <nyu0kim@gmail.com>
2024-03-28 03:58:53 +0900

    Feat: Module에 Typeorm+Mysql 연동을 위한 imports 구문 작성 ConfigModule과 TypeORM 모듈을 작성하고 DB 연동과 관련된 정보는 .env 파일로 분리하여 작성

--- testcrud/src/main.ts
+++ testcrud/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { CrudModule } from './crud/crud.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CrudModule);
   await app.listen(3000);
 }
 bootstrap();
