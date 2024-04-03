a3d2d1eb7a63353e19093322955555f3e1bbf8e3
김난영 <nyu0kim@gmail.com>
2024-04-03 19:17:45 +0900

    Delete younghan-java-start directory

--- younghan-java-start/src/HelloJava2.java
+++
@@ -1,7 +0,0 @@
-public class HelloJava2 {
-    public static void main(String[] args) {
-        System.out.println("Hello Java1");
-        System.out.println("Hello Java2");
-        System.out.println("Hello Java3");
-    }
-}
