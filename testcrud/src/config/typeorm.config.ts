d89f83e4c0e74ff36142a7cffb0fbf8d2d262310
Nyung <nyu0kim@gmail.com>
2024-04-04 10:41:20 +0900

    Feat: 파이정은은 이것을 pull 해서 npm run start 하도록

--- testcrud/src/config/typeorm.config.ts
+++ testcrud/src/config/typeorm.config.ts
@@ -1,15 +1,15 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const port: number = parseInt(<string>process.env.PORT) || 3306;
+// const port: number = parseInt(<string>process.env.PORT) || 3306;
 
-export const typeormConnectionConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: port,
-    username: 'nana',
-    password: '1234',
-    database: 'testcrud_db',
-    entities: ['dist/**/*.entity{.ts,.js'],
-    synchronize: true,
-    timezone: 'utc',
-};
+// export const typeormConnectionConfig: TypeOrmModuleOptions = {
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: port,
+//     username: 'root',
+//     password: '1234',
+//     database: 'testcrud_db',
+//     entities: ['dist/**/*.entity{.ts,.js'],
+//     synchronize: true,
+//     timezone: 'utc',
+// };
