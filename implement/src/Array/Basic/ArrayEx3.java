14f6b7c4821139c8e578bbf0815758e385172d86
Nyung <nyu0kim@gmail.com>
2024-04-22 16:28:59 +0900

    자바의 정석 : 배열의 활용 구현 예제 작성 완료

---
+++ implement/src/Array/Basic/ArrayEx3.java
@@ -0,0 +1,25 @@
+package Array.Basic;
+// 3. 섞기 - 카드섞기
+public class ArrayEx3 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for (int i=0; i<numArr.length; i++) {
+            numArr[i] = i;  // 배열을 0~9의 숫자로 초기화함.
+            System.out.print(numArr[i]);
+        }
+        System.out.println();
+
+        for (int i=0; i<100; i++) {
+            int n = (int)(Math.random() * 10);  // 0~9 중의 한 값을 임의로 얻는다.
+            int tmp = numArr[0];
+            numArr[0] = numArr[n];
+            numArr[n] = tmp;
+        }
+
+        for (int i=0; i<numArr.length; i++) {
+            System.out.print(numArr[i]);
+        }
+
+    }
+}
