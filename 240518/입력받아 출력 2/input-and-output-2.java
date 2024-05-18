e023d693fad53b4e630ddcc439032d20435d467d
김난영 <nyu0kim@gmail.com>
2024-05-18 14:28:05 +0900

    Title: 입력받아 출력 2, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] strArr = str.split("-");
+        System.out.println(strArr[0]+strArr[1]);
+    }
+}
