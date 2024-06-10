30f9e78e9194b0e253da5aa90f914a549507852e
Nyung <nyu0kim@gmail.com>
2024-06-10 22:10:02 +0900

    10번 별찍기 문제 완료

--- book1/10.js
+++ book1/10.js
@@ -1,2 +1,16 @@
 // 문제 10번 : 별 찍기
 
+const n = prompt('숫자를 입력하세요.');
+let tree = '';
+
+for(let i=1; i<=n; i++) {
+    let star = ''; // 한 줄
+    for(let j=1; j<=n-i; j++) {
+        star += ' ';    // 공백
+    }
+    for(let k=1; k<= 2*i; k++) {
+        star += '*';
+    }
+    tree += star + '\n'; 
+}
+console.log(tree);
